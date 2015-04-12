c6250812bd03163c8d1f44e968bd286ac00e59c6
Allan Laal <allan@permanent.ee>
2015-04-12 21:43:16 +0300

    ADD RC1 with calendar event title ordering and custom labeling

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # sunrise
-simple! sunset-sunrise VCALENDAR calendar generator to be used in Google Calendar, your phone and elsewhere
+Simple! sunset-sunrise VCALENDAR calendar generator to be used in Google Calendar, your phone and elsewhere
+
+## usage
+
+Example calendar url:
+http://sun.is.permanent.ee/?latitude=59.4388618469&longitude=24.7544727325&title=sunrise,sunset,length&label_sunrise=↑&label_sunset=↓&start=-100&end=365
+
+Modify the parameters in the url according to your needs. You will atleast need to modify latitude and longitude.
+
+### latitude
+The latitude of your location, in degrees.
+
+### longitude
+The longitude of your location, in degrees.
+
+### title
+This sets what the calendar events title will be, you can use any of the below variables in any order:
+
+astronomical_twilight_begin		Start of astronomical twilight
+nautical_twilight_begin			Start of nautical twilight
+civil_twilight_begin			Start of civil twilight
+sunrise							Sunrise
+transit							Noon
+sunset							Sunset
+civil_twilight_end				End of civil twilight
+nautical_twilight_end			End of nautical twilight
+astronomical_twilight_end		End of astronomical twilight
+length							Length of time from Sunrise until Sunset
+
+### labels
+Normally these values are not prefixed with any text to conserve space, but you can change that by adding an URL parameter with the variable name prefixed with 'label_', for example:
+&astronomical_twilight_end=End%20of%20twilight
+
+### start
+What day to start the calendar on from today.
+
+### end
+What day to end the calendar on from today.
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @desc simple! sunset-sunrise VCALENDAR calendar generator to be used in Google Calendar, your phone and elsewhere
+ * @since 2015-04-12
+ * @author Allan Laal <allan@permanent.ee>
+ * @example http://sun.is.permanent.ee/?latitude=59.4388618469&longitude=24.7544727325&title=sunrise,sunset,length&label_sunrise=↑&label_sunset=↓&start=-100&end=365
+ * @link https://github.com/allanlaal/sunrise
+ */
+$version = '20150401T000000Z'; // modify this when you make changes in the code!
+$default_latitude = 59.4388618469;
+$default_longitude = 24.7544727325;
+
+function param($name, $default='')
+{
+	echo "&$name=$default"; // builds URL parameters with the default values
+	
+	if (
+		isset($_GET[$name])
+		&& 
+		!empty($_GET[$name])
+	)
+	{
+		$out = filter_input(INPUT_GET, $name, FILTER_SANITIZE_STRING);
+	}
+	else
+	{
+		$out = $default;
+	}
+	
+	return $out;
+}
+
+
+$out = ''; // buffer output so if anything fails, it wont display a partial calendar
+$out .= '
+BEGIN:VCALENDAR
+PRODID:-//Permanent Solutions OÜ//Sunrise Sunset Calendar//EN
+VERSION:5.1
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-TIMEZONE:UTC
+';
+
+$now = date('Y-m-d', time());
+for ($day=param('start', 0); $day<=param('end', 365); $day++)
+{
+	$out .= "BEGIN:VEVENT\n";
+	$out .= "DTSTART;VALUE=DATE:".date('Ymd', strtotime($now.' +'.$day.' days'))."\n";
+	$out .= "DTEND;VALUE=DATE:".date('Ymd', strtotime($now.' +'.($day+1).' days'))."\n";
+	$out .= "DTSTAMP:".date('Ymd\THis\Z')."\n";
+	$out .= "UID:Permanent-Sunrise-".date('Ymd', strtotime($now.' +'.$day.' days'))."\n";
+	$out .= "CLASS:PUBLIC\n";
+	$out .= "CREATED:$version\n";
+	$out .= "GEO:".param('latitude', $default_latitude).','.param('longitude', $default_longitude)."\n"; //@see http://tools.ietf.org/html/rfc2445
+
+	$sun_info = date_sun_info(strtotime($now.' +'.$day.' days'), param('latitude', $default_latitude), param('longitude', $default_longitude));
+
+	$out .= 'DESCRIPTION:';
+		$out .= date('H:i', $sun_info['astronomical_twilight_begin']).	' Start of astronomical twilight\n\n';
+		$out .= date('H:i', $sun_info['nautical_twilight_begin']).		' Start of nautical twilight\n\n';
+		$out .= date('H:i', $sun_info['civil_twilight_begin']).		' Start of civil twilight\n\n';
+		$out .= date('H:i', $sun_info['sunrise']).						' Sunrise\n\n';
+		$out .= date('H:i', $sun_info['transit']).						' Noon\n\n';
+		$out .= date('H:i', $sun_info['sunset']).						' Sunset\n\n';
+		$out .= date('H:i', $sun_info['civil_twilight_end']).			' End of civil twilight\n\n';
+		$out .= date('H:i', $sun_info['nautical_twilight_end']).		' End of nautical twilight\n\n';
+		$out .= date('H:i', $sun_info['astronomical_twilight_end']).	' End of astronomical twilight\n\n';
+	$out .= "\n";
+
+	$out .= "LAST-MODIFIED:$version\n";
+	$out .= "SEQUENCE:0\n";
+	$out .= "STATUS:CONFIRMED\n";
+	
+	$out .= "SUMMARY:";
+		foreach (explode(',', param('title', 'sunrise,sunset,length')) as $title)
+		{
+			if ($title == 'length')
+			{
+				$day_length = $sun_info['sunset'] - $sun_info['sunrise'];
+				$day_lenth_h = round($day_length/60/60);
+				$day_lenth_min = round(($day_length - $day_lenth_h*60*60)/60);
+
+				$out .= param('label_length', "")."{$day_lenth_h}h$day_lenth_min";
+			}
+			else
+			{
+				$out .= param('label_'.$title).date('H:i', $sun_info[$title]);
+			}
+			$out .= ' ';
+		}
+	$out .= "\n";
+	
+	$out .= "TRANSP:OPAQUE\n";
+	$out .= "END:VEVENT\n";
+}
+
+$out .= 'END:VCALENDAR';
+
+
+header('Content-type: text/calendar; charset=utf-8');
+header('Content-Disposition: inline; filename=sunrise.ics');
+echo $out;
